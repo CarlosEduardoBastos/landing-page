7d7ccbbf325f6b08dbb04f2c48b281567bd68195
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2022-12-05 01:40:04 -0300

    GridTwoColumns

---
+++ src/components/GridTwoColumns/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid Two Columns',
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, sed doloribus dignissimos sit maxime est quo alias quae in magni voluptatum natus dolor expedita laudantium similique voluptate nam aut enim!`,
+  srcImg: 'assets/images/javascript.svg',
+};
