f1a8fac13788812996db44774ac9a0fdfb8c71c5
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2022-12-09 04:23:38 -0300

    GridImage

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Heading } from '../../components/Heading';
 import { Base } from '../Base';
-import { mockBase } from '../Base/stories';
+import { mockBase } from '../Base/mock';
 import * as Styled from './styles';
 
 export function Home() {
